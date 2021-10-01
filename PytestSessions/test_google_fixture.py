7c8db49639e137f18e354a044163a36b50d0e473
Revanth <sairevanthv8@gmail.com>
2021-10-01 16:24:53 +0530

    fixtures

---
+++ PytestSessions/test_google_fixture.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+import pytest
+
+driver = None
+
+@pytest.fixture(scope='module')
+def init_driver():
+    global driver
+    print("-------------setup----------------")
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.implicitly_wait(10)
+    driver.delete_all_cookies()
+    driver.get("http://www.google.com")
+
+    yield
+    print("-------------tear down----------------")
+    driver.quit()
+
+def test_google_title(init_driver):
+    assert driver.title == "Google"
+
+def test_google_url(init_driver):
+    assert driver.current_url == "https://www.google.com/?gws_rd=ssl"
